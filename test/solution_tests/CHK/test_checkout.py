b6c2688476c91d6d28fb5872d22568d322d7dab6
sbx_user1051 <sbx_user1051@169.254.29.29>
2024-09-21 14:19:31 +0000

    Sat Sep 21 14:19:31 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -8,3 +8,12 @@ class TestCheckout():
         assert checkout_solution.checkout('AAA') == 130
         assert checkout_solution.checkout('AAAA') == 180
         assert checkout_solution.checkout('B') == 30
+        assert checkout_solution.checkout('BB') == 45
+        assert checkout_solution.checkout('C') == 20
+        assert checkout_solution.checkout('D') == 15
+        assert checkout_solution.checkout('ABCD') == 115
+        assert checkout_solution.checkout('AAABBBCCCDDD') == 310
+
+    def test_checkout_invalid(self):
+        assert checkout_solution.checkout('E') == -1
+        assert checkout_solution.checkout('AABR') == -1
