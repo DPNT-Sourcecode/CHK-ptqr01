93a3f74e578f206db0a0734828dcdb45576cd2ce
sbx_user1051 <sbx_user1051@169.254.29.29>
2024-09-21 14:07:31 +0000

    Sat Sep 21 14:07:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,11 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str) -> int:
+    """
+    Calculate the total checkout value for the given SKUs.
+
+    Args:
+        skus (str): A string containing 
+    """
     raise NotImplementedError()
+
