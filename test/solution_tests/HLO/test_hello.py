8c02f70e4233dc969dc05fbbaf96324f72a59bb2
sbx_user1051 <sbx_user1051@169.254.29.29>
2024-09-21 14:04:01 +0000

    Sat Sep 21 14:04:01 UTC 2024

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,8 +3,11 @@ from lib.solutions.HLO import hello_solution
 
 class TestHello():
     def test_hello_world(self):
-        assert hello_solution.hello('any_name') == 'Hello, World!'
+        assert hello_solution.hello('any_name') == 'Hello, any_name!'
 
     def test_hello_friend(self):
         assert hello_solution.hello('John') == 'Hello, John!'
+        assert hello_solution.hello('Jane') == 'Hello, Jane!'
+        assert hello_solution.hello('Kirill') == 'Hello, Kirill!'
+
 
