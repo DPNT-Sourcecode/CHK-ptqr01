13de75f72fc28bdb0128d21127a84b62f87a20b6
sbx_user1051 <sbx_user1051@169.254.29.29>
2024-09-21 14:53:58 +0000

    Sat Sep 21 14:53:58 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,14 +2,19 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout():
-    def test_checkout_valid(self):
-        # Simple single item tests
+    def test_single_items_no_offers(self):
         assert checkout_solution.checkout('A') == 50
         assert checkout_solution.checkout('B') == 30
         assert checkout_solution.checkout('C') == 20
         assert checkout_solution.checkout('D') == 15
+        assert checkout_solution.checkout('E') == 40
+        assert checkout_solution.checkout('F') == 10
+
+    def test_multiple_items_no_offers(self):
+        assert checkout_solution.checkout('ABCD') == 115
 
 
+        # Multi-item tests with offers applied
         assert checkout_solution.checkout('AA') == 100
         assert checkout_solution.checkout('AAA') == 130
         assert checkout_solution.checkout('AAAA') == 180
@@ -17,7 +22,7 @@ class TestCheckout():
         assert checkout_solution.checkout('BB') == 45
         
         
-        assert checkout_solution.checkout('ABCD') == 115
+        
         assert checkout_solution.checkout('AAABBBCCCDDD') == 310
 
     def test_checkout_with_e(self):
@@ -34,4 +39,5 @@ class TestCheckout():
 
     def test_checkout_invalid(self):
         assert checkout_solution.checkout('AABR') == -1
+
 
