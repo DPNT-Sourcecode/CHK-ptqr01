f6ac7874c2d0da9ae215d6f64ee8190690ba99c0
sbx_user1051 <sbx_user1051@169.254.29.29>
2024-09-21 13:50:23 +0000

    Sat Sep 21 13:50:23 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,15 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
-    raise NotImplementedError()
+    """
+    Compute the sum of two positive integers.
+
+    Args:
+        x (int): A positive integer between 0 and 100.
+        y (int): A positive integer between 0 and 100.
+
+    Returns:
+        int: The sum of the two integers.
+    """
+    raise x + y
+
 
